9dd8fc8bfeae9ebfa308d0414ac42ace838516a4
Danishwer <dannywits2003@gmail.com>
2026-08-12 00:11:03 +0530

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.cpp
@@ -0,0 +1,24 @@
+class MinStack {
+    vector<int> minList;
+    vector<int> v;
+
+   public:
+    MinStack() {}
+
+    void push(int val) {
+        if (val <= getMin()) minList.push_back(val);
+        v.push_back(val);
+    }
+
+    void pop() {
+        if (top() == getMin()) minList.pop_back();
+        v.pop_back();
+    }
+
+    int top() { return v[v.size() - 1]; }
+
+    int getMin() {
+        if (minList.empty()) return INT_MAX;
+        return minList[minList.size() - 1];
+    }
+};
